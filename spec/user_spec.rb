4183d1ff7b6a8defba138538ab9aa4bc234037a8
Matt Chan & Vlad Abacumov <pair+matt16749.vabacumov@devbootcamp.com>
2014-08-27 13:47:02 -0700

    adds strong parameters to allow for saving of new user

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "the root view" do
+describe "how to access a user's profile" do
   before do
     User.create(name: 'Matt', age: 25)
   end
@@ -16,4 +16,31 @@ describe "the root view" do
     expect(page).to have_content 'Matt'
     expect(page).to have_content '25'
   end
+
+  it "allows you to go back to the homepage" do
+    visit '/users'
+    click_link 'Matt'
+    click_link 'Back'
+    expect(page).to have_link 'Matt'
+  end
 end
+
+describe "creation of new user" do
+  it "takes us to a form" do
+    visit '/users'
+    click_link 'Create New User'
+    expect(page).to have_content 'Create a new user'
+    expect(page).to have_content 'Name'
+    expect(page).to have_content 'Age'
+    expect(page).to have_button 'Create!'
+  end
+
+  it "allows us to submit the new user form" do
+    visit '/users'
+    click_link "Create New User"
+    fill_in "Name", with: "Vlad"
+    fill_in "Age", with: "25"
+    click_button "Create!"
+    expect(page).to have_content "Vlad"
+  end
+ end
