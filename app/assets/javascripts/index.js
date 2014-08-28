f52d09a127a9647c2e5bed0888c82651e8fd6f42
Vlad Abacumov <vabacumov@gmail.com>
2014-08-28 12:30:20 -0700

    experiments with form tags

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,13 @@
+$(document).on("page:change" ,function() {
+
+ var htmlTags = {
+  this.greeter : $('.greeter'),
+  body : $('body')
+ }
+
+  var hello = function(){
+    this.html("Hello! Welcome to Asset Pipeline");
+  }
+
+
+})
